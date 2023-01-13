04fe112996755d445e43a5b3816629d99e4ffb56
arthurPieniz <102185959+arthurPieniz@users.noreply.github.com>
2023-01-13 11:36:10 -0300

    upload

---
+++ arquivo.js
@@ -0,0 +1,45 @@
+let pessoas = {
+    pessoa1:{
+        nome:"Cláudio Pereira",
+        email:"claudio.pereira@gmail.com",
+        senha:"smart2020"
+    },
+    pessoa2:{
+        nome:"Butui Legal",
+        email:"butui.legal@gmail.com",
+        senha:"butui123"
+    },
+    pessoa3:{
+        nome:"Luca Naco",
+        email:"luca.naco@gmail.com",
+        senha:"timedosboni"
+    },
+    pessoa4:{
+        nome:"Bezinho Cria",
+        email:"bezinho.cria@gmail.com",
+        senha:"godofredovsk"
+    },
+    pessoa5:{
+        nome:"",
+        email:"@gmail.com",
+        senha:""
+    },
+}
+
+let historias={
+    hist1:{
+        nome:"Os três porquinhos",
+        descricao:"Os três porquinhos saem da casa de sua mãe e vão constrir cada uma a sua própria casa, porém havia um lobo faminto que morava ali por perto",
+        historia:"Era uma vez 3 porquinhos, eles moravam com a sua mãe. Certo dia a mãe deles falou que eles estavam grandinhos já e precisavam construir sua própria casa. Então eles foram atrás dos materias para fazer a casa, o irmão caçula era o mais preguiçoso, e por isso acabu faendo uma fragil casa de palha. O irmão do meio se esforçou um pouco mais do que o mais novo e fez uma casa de madeira. o irmão mais velho, que era mais dedicado fez uma resistente casa de tijolos. Após isso um lobo faminto e malvado buscava comida, e os porquinhos foram a sua escolha começou pelo mais novo. O lobo disse abra essa porta! O porquinho recusou, mas o lobo não aceitou e soprou e soprou e a casa voou, o porquinho desesperado correu para a casa do irmão do meio. Mas lá estava o lobo novamete que soprou e soprou e sopru e soprou, a casa também não resistiu e voou, os dois irmão desesperados correram para a casa de tijolo do irmão mais velho. Porém novamente e mais famnto chegava o logo, só que, desta vez o lobo soprou e soprou, mas não conseguiu fazer a casa voar. Ele pensou e pensou e tentou invadir a casa pela chaminé, mas a chaminé estava com o fogo aceso e o lobo queimou o rabo e foi ebora e nunca mais foi visto."
+    },
+    hist2:{
+        nome:"A história de Godofredo",
+        descricao:"O Godo era um cueio muito pobre até que a mãe dele expulsou e um homem rico apareceu.",
+        historia:"Era uma vez um cueio chamado Godo ele era muito pobre e morava com sua mãe até que um dia passou um homem que era muito rico mas ele foi embora até que no outro dia a mãe de Godo expulsou ele de casa e o mesmo homem apareceu e levou o Godo para sua casa daí o Godo perguntou qual é seu nome daí o homem respondeu Menino Be daí o Godo morou com o Menino Be e eles ficaram felizes pra sempre."
+    },
+    hist3:{
+        nome:"Como o Boni nasceu",
+        descricao:"Boni nasceu com espírito abençoado. E hoje em dia Boni é famoso por muitos e reconhecido por muitos.",
+        historia:"Em um belo dia uma mulher grávida ganhou lindo filho que cresceu muito rápido.O nome de sua mãe era Bonona que nasceu em um minúsculo país que se chamava país dos Fofuchinhos.Quando Boni já estava grande, acabou namorando uma mulher chamada Bona.Boni encontrou uma organização com o Fofuchinho presidente do país, Eleforte,Elefote e Luca.Eles planejavam fazer um clube para participar dos esportes, principalmente o futebol e o Boni decidiu entrar para a organização...meses depois,ele conseguiram criar as equipes dos timesdos esportes e do futebol que seriam eles, ainda conseguiram expandir o país.Os times ficaram com o nome Time dos Boni.E o hoje em dia Boni é famoso por muitos respeitado por muitos."
+    },
+}
